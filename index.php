8be1cc61c8dd50c0d698faeba0eade361f5086a6
zadeloumar <122491995+zadeloumar@users.noreply.github.com>
2025-04-12 05:59:44 +0500

    Первый коммит проекта с видео

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/tailwind_style.css ">
+    <link rel="stylesheet" href="./css/style_video.css">
+    <!-- <link href="https://vjs.zencdn.net/7.20.3/video-js.css" rel="stylesheet" /> -->
+    <title>Document</title>
+</head>
+<body class="bg-[#bebebe]">
+  <header class="">
+    
+    <div class="bg-white rounded-xl m-2 p-1 container mx-auto w-full flex justify-between">
+      <div class="video_list w-[90%] flex ">
+        <!-- style_video -->
+        <details class="details w-full pt-2">
+          <summary class="details__title w-full flex items-center gap-6">
+            <div href="" class="active flex items-center justify-center bg-[#e06b1c] h-10  w-10 p-2  rounded-lg ">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi-play-fill" viewBox="0 0 16 16">
+                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"></path>
+               </svg>
+            </div>
+            <span>level 1</span>
+          </summary>
+          <div class="details__content">
+              <div class="details__element flex items-center mb-2 gap-6">
+                  <div href="" class="flex items-center justify-center bg-[#e06b1c] w-10 h-10 p-2  rounded-lg border-1 border-[#0e30f0]">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi-play-fill" viewBox="0 0 16 16">
+                      <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"></path>
+                    </svg>
+                  </div>
+                  <span>Раскрыва</span> 
+              </div>
+              <div class="details__element flex items-center mb-2 gap-6">
+                <div href="" class="flex items-center justify-center bg-[#e06b1c] w-10 h-10 p-2  rounded-lg border-1 border-[#0e30f0]">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi-play-fill" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"></path>
+                  </svg>
+                </div>
+                <span>Раскрыва</span> 
+              </div>
+              <div class="details__element flex items-center mb-2 gap-6">
+                <div href="" class="flex items-center justify-center bg-[#e06b1c] w-10 h-10 p-2  rounded-lg border-1 border-[#0e30f0]">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi-play-fill" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"></path>
+                  </svg>
+                </div>
+                <span>Раскрыва</span> 
+              </div>
+              <div class="details__element flex items-center mb-2 gap-6">
+                <div href="" class="flex items-center justify-center bg-[#e06b1c] w-10 h-10 p-2  rounded-lg border-1 border-[#0e30f0]">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi-play-fill" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"></path>
+                  </svg>
+                </div>
+                <span>Раскрыва</span> 
+              </div>
+              <div class="details__element flex items-center mb-2 gap-6">
+                <div href="" class="flex items-center justify-center bg-[#e06b1c] w-10 h-10 p-2  rounded-lg border-1 border-[#0e30f0]">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi-play-fill" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"></path>
+                  </svg>
+                </div>
+                <span>Раскрыва</span> 
+              </div>
+              <div class="details__element flex items-center mb-2 gap-6">
+                <div href="" class="flex items-center justify-center bg-[#e06b1c] w-10 h-10 p-2  rounded-lg border-1 border-[#0e30f0]">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi-play-fill" viewBox="0 0 16 16">
+                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"></path>
+                  </svg>
+                </div>
+                <span>Раскрыва</span> 
+              </div>
+              
+             
+          </div>          
+        </details>
+        
+      </div>
+      <div class="btn_exsit h-4/5 flex p-2 ">
+        <a class="bg-[#f39c12] p-2 rounded-lg text-center" href="">Выход</a>
+      </div>
+    </div>
+  </header>
+  <header class="">
+    
+    
+  </header>
+  <section class="text-gray-600 body-font relative">
+    <div class="container sm:px-5 py-4 mx-auto sm:flex">
+      <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 flex items-end justify-start relative">
+        
+
+      </div>
+      <div class=" lg:w-1/3 md:w-1/3 bg-white flex flex-col p-2 rounded-xl md:ml-auto  h-full  md:py-8 mt-8 md:mt-0 ">
+        <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+        <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat illum quaerat quisquam alias nisi facilis velit? Sit amet excepturi officiis perspiciatis maxime sapiente, tempore repellendus consequuntur nam sint nesciunt libero?</p>
+        
+    </div>
+  </section>
+  
+  <script src="https://cdn.jsdelivr.net/npm/shaka-player@3.1.0/dist/shaka-player.compiled.js"></script>
+
+<script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
+<script>
+     var player = videojs('videoPlayer');
+     navigator.mediaDevices.getDisplayMedia().then(() => {
+ document.querySelector("video").style.filter = "blur(90px)";
+ });
+
+</script> 
+
+</body>
+</html>
